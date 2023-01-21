cd840215e99aa6c4ee1de5e8900d0975fe46ddc5
Sergio Satyabrata <sergio.satyabrata@gmail.com>
2023-01-20 21:27:27 -0800

    First commit

---
+++ myshell.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <sys/wait.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <string>
+
+int main(int argc, char* argv[]){
+	std::cout << "myshell$";
+	std::getline();
+
+	return 0;
+}
